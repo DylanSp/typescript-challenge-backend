274f33ba6f3d676be3ecc0e201a25d8865679143
Dylan Sprague <dylan.richard.sprague@gmail.com>
2021-07-09 15:18:59 -0400

    Cache MongoDB connection as singleton

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,23 @@
-import { MongoClient } from "mongodb";
+import { Db, MongoClient } from "mongodb";
 
-export async function connect() {
+// cached singleton connection
+let database: Db | undefined;
+
+export async function connect(): Promise<Db> {
+  if (database !== undefined) {
+    return database;
+  }
+
   const dbString = process.env.MONGO_URL;
-  console.log(dbString);
+
+  if (dbString === undefined) {
+    throw new Error("Unable to connect to Mongo database");
+  }
   
   const client = new MongoClient(dbString, {
     useUnifiedTopology: true,
   });
   const connection = await client.connect();
-  return connection.db("sample_airbnb");
+  database = connection.db("sample_airbnb");
+  return database;
 }
